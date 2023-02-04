13255fdec1261d81917f056a51c75ab9c95558e6
MiguelGutierrez-MIKE2099 <122950589+MiguelGutierrez-MIKE2099@users.noreply.github.com>
2023-02-03 22:57:11 -0600

    commit reto 2, 1

---
+++ src/edu/miguel/angel/gutierrez/ramirez/reto2/boletos/Main.java
@@ -0,0 +1,4 @@
+package edu.miguel.angel.gutierrez.ramirez.reto2.boletos;
+
+public class Main {
+}
---
+++ src/edu/miguel/angel/gutierrez/ramirez/reto2/piramide/Main.java
@@ -0,0 +1,4 @@
+package edu.miguel.angel.gutierrez.ramirez.reto2.piramide;
+
+public class Main {
+}
