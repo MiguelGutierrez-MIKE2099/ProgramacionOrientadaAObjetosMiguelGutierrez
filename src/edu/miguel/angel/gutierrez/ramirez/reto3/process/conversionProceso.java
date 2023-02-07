98e0a2732d4f43abf4b30d4cc22397dc4ba9ac52
MiguelGutierrez-MIKE2099 <122950589+MiguelGutierrez-MIKE2099@users.noreply.github.com>
2023-02-07 09:32:00 -0600

    commit reto 3, 3

--- src/edu/miguel/angel/gutierrez/ramirez/reto3/process/conversionProceso.java
+++ src/edu/miguel/angel/gutierrez/ramirez/reto3/process/conversionProceso.java
@@ -2,7 +2,7 @@ package edu.miguel.angel.gutierrez.ramirez.reto3.process;
 
 /**
  * @author Miguel Gutiérrez Ramírez
- * @class conversionProceso clase con métodos de conversión de números en diferentes bases numéricas
+ * conversionProceso clase con métodos de conversión de números en diferentes bases numéricas
  */
 public class conversionProceso {
     /**
@@ -11,7 +11,7 @@ public class conversionProceso {
      * @return binario representación en base binaria del número decimal recibido
      */
     public static String covertirDecimalABinario(String decimal){
-
+        System.out.println("ded");
         return decimal;
     }
 
