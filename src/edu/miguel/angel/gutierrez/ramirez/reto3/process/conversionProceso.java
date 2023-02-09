d5330b38b32cd72178b4032c8355d517fc92e681
MiguelGutierrez-MIKE2099 <122950589+MiguelGutierrez-MIKE2099@users.noreply.github.com>
2023-02-09 14:46:33 -0600

    commit reto 3, 5

--- src/edu/miguel/angel/gutierrez/ramirez/reto3/process/conversionProceso.java
+++ src/edu/miguel/angel/gutierrez/ramirez/reto3/process/conversionProceso.java
@@ -1,5 +1,6 @@
 package edu.miguel.angel.gutierrez.ramirez.reto3.process;
 
+
 /**
  * @author Miguel Gutiérrez Ramírez
  * conversionProceso clase con métodos de conversión de números en diferentes bases numéricas
@@ -7,11 +8,21 @@ package edu.miguel.angel.gutierrez.ramirez.reto3.process;
 public class conversionProceso {
     /**
      * covertirDecimalABinarior recibe un número en base decimal para convertirlo a su representación en base binaria
-     * @param decimal número en base decimal a ser convertido en base binaria
+     * @param decBaseNum número en base decimal a ser convertido en base binaria
      * @return binario representación en base binaria del número decimal recibido
      */
-    public static String covertirDecimalABinario(String decimal){
-        return decimal;
+    public static String covertirDecimalABinario(String decBaseNum, short systemBase){
+        return Integer.toString(Integer.parseInt(decBaseNum), systemBase);
+    }
+
+    /**
+     * convertirBinarioaHexadecimal analiza un número binario/octal/hexadecimal para su representación en el sistema decimal
+     */
+    public static String convertirBinarioaHexadecimal
+    (String binOctHexBaseNum, byte systemBase){
+        return Integer.toString(Integer.parseInt(binOctHexBaseNum, systemBase));
     }
 
 }
+
+
